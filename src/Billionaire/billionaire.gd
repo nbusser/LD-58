4a6d3a0115bb1026f0f1aa91f4980a05d79cb589
Nicolas Busser <nicolas.busser67@gmail.com>
2025-10-04 11:38:01 +0900

    refine bullet spawn

--- src/Billionaire/billionaire.gd
+++ src/Billionaire/billionaire.gd
@@ -1,12 +1,14 @@
 class_name Billionaire
 
-extends CharacterBody2D
+extends Node2D
 
 const GRAVITY: float = 1200.0
 
 var _is_gravity_enabled: bool = true
 
 @onready var _idle_timer: Timer = $IdleTimer
+@onready var _body: CharacterBody2D = $BillionaireBody
+@onready var _bullets: Node2D = $Bullets
 
 var _bullet_scene = preload("res://src/Bullet/Bullet.tscn")
 
@@ -30,27 +32,28 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if _is_gravity_enabled:
-		velocity.y += GRAVITY * delta
+		_body.velocity.y += GRAVITY * delta
+	_body.move_and_slide()
 
+func _spawn_bullet(bullet_position: Vector2, bullet_direction: Vector2, bullet_speed: float = 100.0) -> void:
+	var bullet: Bullet = _bullet_scene.instantiate()
+	bullet.init(bullet_position, bullet_direction, bullet_speed)
+	_bullets.add_child(bullet)
 
 func _air_shotgun_routine() -> void:
-	velocity.y = -600
-	while velocity.y < 0:
+	_body.velocity.y = -600
+	while _body.velocity.y < 0:
 		await get_tree().process_frame
 
 	_is_gravity_enabled = false
 	var freeze_timer := get_tree().create_timer(0.5)
-	_is_gravity_enabled = true
-
 	await freeze_timer.timeout
-
-	var bullet: Bullet = _bullet_scene.instantiate()
+	_is_gravity_enabled = true
 	
 	#TODO: get player position
-	var player_position = Vector2(0.0, 0.0)
-	var bullet_direction = player_position - position
-	bullet.init(position, bullet_direction, 200)
-	add_child(bullet)
+	var player_position = Vector2(150.0, 150.0)
+	var bullet_direction = player_position - _body.position
+	_spawn_bullet(_body.position, bullet_direction, 200.0)
 	
 
 func _on_idle_timer_timeout() -> void:
--- src/Bullet/bullet.gd
+++ src/Bullet/bullet.gd
@@ -5,21 +5,22 @@ extends Area2D
 var _direction: Vector2
 var _speed: float
 
+@onready var _sprite = $Sprite2D
+
 var _initialized = false
 func init(start_position: Vector2, direction: Vector2, speed: float = 100) -> void:
 	_initialized = true
-	self.position = start_position
+	position = start_position
 	_direction = direction.normalized()
-	if speed > 0.0:
-		_speed = speed
-	rotation = _direction.angle()
+	_speed = speed
 
 func _ready() -> void:
 	assert(_initialized, "init() must be called")
+	_sprite.rotation = _direction.angle()
 
 func _physics_process(delta: float) -> void:
-	global_position += _direction * _speed * delta
+	position += _direction * _speed * delta
 
 func _on_area_entered(_area: Area2D) -> void:
 	print("HIT")
-	#queue_free()
+	queue_free()
