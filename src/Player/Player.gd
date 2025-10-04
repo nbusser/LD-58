cbdac9de93423f1332a53fab2230caad5bd7b5df
mbty <mbty@protonmail.com>
2025-10-04 00:15:11 +0000

    add background sprite

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -15,6 +15,7 @@ func _ready() -> void:
 func move(velocity_p: Vector2) -> void:
 	set_velocity(velocity_p)
 	move_and_slide()
+	print(velocity_p)
 	_velocity = velocity_p
 	_sprite.rotation = lerp_angle(
 		_sprite.rotation, velocity.angle(), 10.0 * get_physics_process_delta_time()
