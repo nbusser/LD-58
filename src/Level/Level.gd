5ce41c1340e09ce5fe7ed0b3de68131008a5aa07
mbty <mbty@protonmail.com>
2025-10-04 00:52:18 +0000

    remove timer duration override

--- src/Level/Level.gd
+++ src/Level/Level.gd
@@ -12,8 +12,6 @@ func _ready():
 	assert(level_state, "init must be called before creating Level scene")
 	hud.init(level_state)
 
-	timer.start(level_state.level_data.timer_duration)
-
 
 func init(level_number_p: int, level_data_p: LevelData, nb_coins_p: int):
 	level_state = LevelState.new(level_number_p, level_data_p, nb_coins_p)
