cc07444f4cbd66f22030b34e55458e09e7254bfc
mbty <mbty@protonmail.com>
2025-10-04 18:30:13 +0000

    run gdformat

--- src/Player/camera_2d.gd
+++ src/Player/camera_2d.gd
@@ -2,8 +2,8 @@ extends Camera2D
 
 @onready var player = $"../Player"
 
+
 func _process(_delta):
 	position = player.position
-	var zoom_level = clamp(1.0 - player.velocity.length()/2000 + .3, .3, 1.3)
-	zoom = lerp(zoom, Vector2(zoom_level, zoom_level), 2*_delta)
-	
+	var zoom_level = clamp(1.0 - player.velocity.length() / 2000 + .3, .3, 1.3)
+	zoom = lerp(zoom, Vector2(zoom_level, zoom_level), 2 * _delta)
