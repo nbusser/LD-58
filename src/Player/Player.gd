1b23a33341e8ef901b0af20d0fcb1c471e027f68
mbty <mbty@protonmail.com>
2025-10-04 00:41:18 +0000

    remove dead code

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -2,23 +2,11 @@ class_name Player
 
 extends CharacterBody2D
 
-var _velocity := Vector2.ZERO
 @export var speed = 400
-
-@onready var _sprite := $Sprite2D
 
 func _ready() -> void:
 	# Waits for Game.gd to run randomize()
 	await get_tree().process_frame
-
-func move(velocity_p: Vector2) -> void:
-	set_velocity(velocity_p)
-	move_and_slide()
-	print(velocity_p)
-	_velocity = velocity_p
-	_sprite.rotation = lerp_angle(
-		_sprite.rotation, velocity.angle(), 10.0 * get_physics_process_delta_time()
-	)
 
 func get_input():
 	var input_direction = Input.get_axis("move_left", "move_right")
