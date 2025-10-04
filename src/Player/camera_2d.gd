0e5569d3e58129b450bbf6a9cf83d413f5888fea
mbty <mbty@protonmail.com>
2025-10-04 18:37:19 +0000

    make camera follow player less directly

--- src/Player/camera_2d.gd
+++ src/Player/camera_2d.gd
@@ -4,6 +4,8 @@ extends Camera2D
 
 
 func _process(_delta):
-	position = player.position
+	# TODO the code below is framerate dependent, see
+	# https://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+	position = lerp(position, player.position, 10*_delta)
 	var zoom_level = clamp(1.0 - player.velocity.length() / 2000 + .3, .3, 1.3)
 	zoom = lerp(zoom, Vector2(zoom_level, zoom_level), 2 * _delta)
