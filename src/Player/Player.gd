4b961399ddc383e2f803d4693711bd0ebde0d088
Louis MOUHAT <louis.mouhat@gmail.com>
2025-10-04 10:00:35 +0200

    fix jump logic

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -4,47 +4,42 @@ extends CharacterBody2D
 @export var speed = 400
 @export var dash_cooldown = 1.0
 @export var dash_window = .2
+@export var max_input_jump_time = .4
+@export var jump_force = 8000
 
 var jump_load_start = null
-var is_loading_jump = false
+var is_actively_jumping = false
 var previous_left = 0
 var previous_right = 0
 var previous_dash = 0
 
 @onready var _hurt_sound = $SoundFx/HurtSound
-
+var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready() -> void:
 	# Waits for Game.gd to run randomize()
 	await get_tree().process_frame
-
 
 func _physics_process(delta):
 	var input_direction = Input.get_axis("move_left", "move_right")
 	var vt_velocity = 0.
 	var hz_velocity = 0.
 	# Jumping + walking on floor
-	if is_on_floor():
-		hz_velocity = input_direction * speed
-		# TODO the loaded jumps mechanism is bad
-		if !is_loading_jump && Input.is_action_pressed("jump"):
-			is_loading_jump = true
-			jump_load_start = Time.get_unix_time_from_system()
-		elif (
-			is_loading_jump
-			&& (
-				Input.is_action_just_released("jump")
-				|| (Time.get_unix_time_from_system() - jump_load_start) > .2
-			)
-		):
-			is_loading_jump = false
-			var timer_proportion = (
-				.5 + 5. / 2. * clamp(Time.get_unix_time_from_system() - jump_load_start, 0, .2)
-			)
-			vt_velocity = -speed * timer_proportion
-	else:
-		is_loading_jump = false
-		hz_velocity = velocity.x - (.1 * delta * velocity.x)
+	var now = Time.get_unix_time_from_system()
+	hz_velocity = input_direction * speed
+	if is_on_floor() && !is_actively_jumping && Input.is_action_pressed("jump"):
+		is_actively_jumping = true
+		jump_load_start = now
+
+	var time_since_jump = now - (jump_load_start if jump_load_start != null else +INF)
+	if is_actively_jumping && Input.is_action_pressed("jump") && time_since_jump < max_input_jump_time:
+		var timer_proportion = (
+			1 - clamp(time_since_jump, 0, max_input_jump_time) / max_input_jump_time
+		) ** 4
+		vt_velocity = -jump_force * delta * timer_proportion
+	elif is_actively_jumping && (time_since_jump > max_input_jump_time || Input.is_action_just_released("jump")):
+		is_actively_jumping = false
+
 	# Dashing
 	if Input.is_action_just_pressed("move_left"):
 		var ts = Time.get_unix_time_from_system()
@@ -65,7 +60,7 @@ func _physics_process(delta):
 		if abs(velocity.x) > abs(hz_velocity)
 		else hz_velocity
 	)
-	velocity = Vector2(hz_velocity, velocity.y + vt_velocity + 980 * delta)
+	velocity = Vector2(hz_velocity, velocity.y + vt_velocity + GRAVITY * delta)
 	move_and_slide()
 
 
