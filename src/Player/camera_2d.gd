a8e35f778890e4a5467affd121078ea32c44c844
mbty <mbty@protonmail.com>
2025-10-05 09:30:24 +0000

    tweak camera

--- src/Player/camera_2d.gd
+++ src/Player/camera_2d.gd
@@ -47,8 +47,8 @@ func get_noise_offset(delta: float) -> Vector2:
 func _process(delta):
 	# TODO the code below is framerate dependent, see
 	# https://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
-	position = lerp(position, player.position, 10 * delta)
-	var zoom_level = clamp(1.0 - player.velocity.length() / 2000 + .3, .3, 1.3)
+	position = lerp(position, player.position, 6 * delta)
+	var zoom_level = clamp(1.0 - player.velocity.length() / 400 + .3, .3, 1.8)
 	zoom_level += 2
 	zoom = lerp(zoom, Vector2(zoom_level, zoom_level), 2 * delta)
 
