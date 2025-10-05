bf9b54be886475bad66818aa4df19a529063a9b0
mbty <mbty@protonmail.com>
2025-10-05 16:24:31 +0000

    fix camera showing out of bounds

--- src/Player/camera_2d.gd
+++ src/Player/camera_2d.gd
@@ -97,7 +97,7 @@ func _process(delta):
 		global_position = lerp(
 			position, player.global_position - player_billionaire_dist / 2, 100 * delta
 		)
-		var zoom_level = 2 - 1.2 * clamp(player_billionaire_dist.length() / 800, 0., 1.)
+		var zoom_level = 2 - .9 * clamp(player_billionaire_dist.length() / 800, 0., 1.)
 		zoom = lerp(zoom, Vector2(zoom_level, zoom_level), 2 * delta)
 	else:
 		position = lerp(position, player.position + Vector2(0, -35), 12 * delta)
