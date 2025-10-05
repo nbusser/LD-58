d0f334af42d93ba1e36013ffa6df2b23e83c59b1
mbty <mbty@protonmail.com>
2025-10-05 01:25:27 +0000

    increase zoom level

--- src/Player/camera_2d.gd
+++ src/Player/camera_2d.gd
@@ -49,6 +49,7 @@ func _process(delta):
 	# https://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
 	position = lerp(position, player.position, 10 * delta)
 	var zoom_level = clamp(1.0 - player.velocity.length() / 2000 + .3, .3, 1.3)
+	zoom_level += 2
 	zoom = lerp(zoom, Vector2(zoom_level, zoom_level), 2 * delta)
 
 	# Fade out the intensity over time
