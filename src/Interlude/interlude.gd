1eb33390031e9fa16abdabb7d8754093c3dec5b0
Antoine Brûlé <antoine.brule@netc.eu>
2025-10-05 23:20:27 +0200

    add recap at end of level

--- src/Interlude/interlude.gd
+++ src/Interlude/interlude.gd
@@ -2,15 +2,22 @@ class_name Interlude
 
 extends Control
 
+var statement_line_scene = preload("res://src/Interlude/statement_line.tscn")
+
+@onready var statement_lines_container: VBoxContainer = %StatementLinesContainer
+@onready var panel = $CenterContainer/Panel
+@onready var subtitle_label: Label = %SubtitleLabel
+@onready var total_value_label: Label = %TotalValueLabel
+
 
 func _blocking_dialog(text: String):
-	$Panel.visible = true
-	$Panel/VBoxContainer/Label.text = text
+	panel.visible = true
+	subtitle_label.text = text
 	while true:
 		await get_tree().process_frame
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			break
-	$Panel.visible = false
+	panel.visible = false
 
 
 func _cutscene():
@@ -30,7 +37,6 @@ func _cutscene():
 	await $UpgradeSelector.close
 
 	await get_tree().create_timer(0.1).timeout
-	await _blocking_dialog("Tro kool")
 
 	await (
 		get_tree()
@@ -43,10 +49,40 @@ func _cutscene():
 
 
 func _ready():
+	_setup_statement_lines()
+
 	$Player.position = Vector2(-68.0, 417.0)
-	$Panel.visible = false
+	panel.visible = false
 	$UpgradeSelector.visible = false
 
 	await _cutscene()
 
 	Globals.end_scene(Globals.EndSceneStatus.INTERLUDE_END)
+
+
+func _setup_statement_lines():
+	for child in statement_lines_container.get_children():
+		child.queue_free()
+
+	for collectible_type in Collectible.CollectibleType.values():
+		if collectible_type not in GameState.latest_level_state.collected_items:
+			continue
+
+		var value = GameState.latest_level_state.collected_items[collectible_type]
+
+		if value <= 0:
+			continue
+
+		var statement_line: StatementLine = statement_line_scene.instantiate()
+		var unit_value = Collectible.get_collectible_value(collectible_type)
+		(
+			statement_line
+			. init(
+				Collectible.get_collectible_name(collectible_type),
+				value,
+				unit_value,
+			)
+		)
+		statement_lines_container.add_child(statement_line)
+
+	total_value_label.text = "$%d" % GameState.latest_level_state.get_value_of_collected_items()
---
+++ src/Interlude/statement_line.gd
@@ -0,0 +1,27 @@
+class_name StatementLine
+
+extends HBoxContainer
+
+var _description: String = ""
+var _quantity: int = 0
+var _value: int = 0
+var _unit_value: int = 0
+
+@onready var description_label: Label = %DescriptionLabel
+@onready var quantity_label: Label = %QuantityLabel
+@onready var unit_value_label: Label = %UnitValueLabel
+@onready var value_label: Label = %ValueLabel
+
+
+func init(description: String, quantity: int, unit_value: int) -> void:
+	_description = description
+	_quantity = quantity
+	_unit_value = unit_value
+	_value = quantity * unit_value
+
+
+func _ready() -> void:
+	description_label.text = _description
+	quantity_label.text = str(_quantity)
+	unit_value_label.text = "$%d" % _unit_value
+	value_label.text = "$%d" % _value
