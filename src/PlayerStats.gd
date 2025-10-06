a58a7db45ef9b481896f24588fd9ac3a5c66e814
mbty <mbty@protonmail.com>
2025-10-06 19:15:18 +0000

    start with no unlocks

--- src/PlayerStats.gd
+++ src/PlayerStats.gd
@@ -1,18 +1,18 @@
 class_name PlayerStats
 extends Resource
 
-var unlocked_dash = true
-var unlocked_dash_down = true
-var unlocked_wall_climbing = true
+var unlocked_dash = false
+var unlocked_dash_down = false
+var unlocked_wall_climbing = false
 var unlocked_wall_projection = false
-var unlocked_dash_bullet_time = true
+var unlocked_dash_bullet_time = false
 var unlocked_on_demand_bullet_time = false
-var unlocked_bonus_jump_after_airhit = true
+var unlocked_bonus_jump_after_airhit = false
 var unlocked_fast_cooldown_dash = false
-var unlocked_stronger_dash = true
-var unlocked_higher_jumps = true
-var unlocked_dash_glide = true
-var max_nb_jumps = 2
+var unlocked_stronger_dash = false
+var unlocked_higher_jumps = false
+var unlocked_dash_glide = false
+var max_nb_jumps = 1
 var interest_rate = 0
 # Bullet proximity slow motion parameters
 var bullet_proximity_radius = 65.0
