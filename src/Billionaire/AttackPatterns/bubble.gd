7c9a73255b1cc9230afb5e757e135b8290aad2aa
Nicolas Busser <nicolas.busser67@gmail.com>
2025-10-09 20:32:37 +0900

    add tool to update size of bubbles

--- src/Billionaire/AttackPatterns/bubble.gd
+++ src/Billionaire/AttackPatterns/bubble.gd
@@ -1,12 +1,13 @@
+@tool
 class_name Bubble
 
 extends Area2D
 
 enum Size { SMALL, MEDIUM, LARGE }
 
-const _EXPLOSION_FRAME = 37
+@export_tool_button("Update Preview Size") var update_preview_size_action = _update_preview_size
 
-@export var size: Size = Size.SMALL
+@export var bubble_size: Size = Size.SMALL
 
 @onready var _bubble_anim_resource_dict: Dictionary[Size, SpriteFrames] = {
 	Size.SMALL: preload("res://src/Billionaire/bubble_100.tres"),
@@ -14,17 +15,33 @@ const _EXPLOSION_FRAME = 37
 	Size.LARGE: preload("res://src/Billionaire/bubble_300.tres")
 }
 
+var _bubble_preview_frame_dict: Dictionary[Size, int] = {
+	Size.SMALL:  48,
+	Size.MEDIUM: 49,
+	Size.LARGE: 49
+}
+
+var _bubble_explosion_frame_dict: Dictionary[Size, int] = {
+	Size.SMALL: 37,
+	Size.MEDIUM: 38,
+	Size.LARGE: 38
+}
+
 @onready var _sprite: AnimatedSprite2D = $Sprite
 
 
 func _reset():
 	visible = false
+	_sprite.frame = 0
+
 	monitorable = false
 	monitoring = false
 
 
 func _ready() -> void:
-	_sprite.sprite_frames = _bubble_anim_resource_dict[size]
+	$EditorPreviewSprite.visible = false
+	_sprite.sprite_frames = _bubble_anim_resource_dict[bubble_size]
+	_reset()
 
 
 func spawn(free_at_the_end: bool = false) -> void:
@@ -32,7 +49,7 @@ func spawn(free_at_the_end: bool = false) -> void:
 	_sprite.play("default")
 
 	# Waiting for bubble to explode
-	while _sprite.frame != _EXPLOSION_FRAME:
+	while _sprite.frame != _bubble_explosion_frame_dict[bubble_size]:
 		await _sprite.frame_changed
 
 	$ExplosionSound.play()
@@ -51,3 +68,8 @@ func spawn(free_at_the_end: bool = false) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group(Globals.GROUPS_DICT[Globals.Groups.PLAYER]):
 		(body as Player).get_hurt(Vector2(0, 0))
+
+func _update_preview_size() -> void:
+	visible = true
+	_sprite.sprite_frames = _bubble_anim_resource_dict[bubble_size]
+	_sprite.frame = _bubble_preview_frame_dict[bubble_size]
