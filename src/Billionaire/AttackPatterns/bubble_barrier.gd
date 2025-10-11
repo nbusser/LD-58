5a6f98b1f5bfc32d5c3fd75e48e9b1c985d4d85f
Nicolas Busser <nicolas.busser67@gmail.com>
2025-10-11 20:14:00 +0900

    make bubble barrier more violent

--- src/Billionaire/AttackPatterns/bubble_barrier.gd
+++ src/Billionaire/AttackPatterns/bubble_barrier.gd
@@ -1,6 +1,9 @@
+@tool
 class_name BubbleBarrier
 
 extends Node2D
+
+@export_tool_button("Refresh Bubble Preview") var refresh_bubble_previews = _refresh_bubble_previews
 
 var _is_running = false
 
@@ -24,3 +27,9 @@ func spawn(spawn_interval: float):
 	await get_tree().create_timer(1.0).timeout
 
 	_is_running = false
+
+
+func _refresh_bubble_previews() -> void:
+	for wave: Node2D in _waves.get_children():
+		for bubble: Bubble in wave.get_children():
+			bubble.update_preview_size_action.call()
